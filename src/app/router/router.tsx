66ec791b52741634b1bc33e3383e957a8649a6f4
MINJOO-KIM <gkminju2000@gmail.com>
2025-09-28 18:13:33 +0900

    feat: 기본 라우터 구조 설정

---
+++ src/app/router/router.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+import { LandingPage } from '../../pages/landing'
+import { HomePage } from '../../pages/home'
+import { NewsPage } from '../../pages/news'
+import { AdminPage } from '../../pages/admin'
+
+// import { AuthGuard, AdminGuard, AdditionalInfoGuard } from '@/routes/guards';
+// import { MainLayout } from '@/routes/layouts/MainLayout';
+
+const AppRoutes = () => (
+  <Routes>
+    <Route path='/' element={<LandingPage />} />
+
+    {/* 로그인 후 접근 가능 페이지 */}
+    <Route path='/home' element={<HomePage />} />
+    <Route path='/news' element={<NewsPage />} />
+
+    {/* 관리자 페이지 */}
+    <Route path='/admin' element={<AdminPage />} />
+  </Routes>
+)
+
+export default AppRoutes
