2a22dea0c8166d35bd8e701bf418b5b88d7b8b02
MINJOO-KIM <gkminju2000@gmail.com>
2025-10-02 00:41:43 +0900

    design: pretendard 폰트 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import '@/app/styles/index.scss'
+import '@fontsource/pretendard'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
