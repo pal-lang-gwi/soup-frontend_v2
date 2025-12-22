5e93947be5f4f1f05c6c3590aaf52ea50c06e85c
MINJOO-KIM <gkminju2000@gmail.com>
2025-12-22 22:54:47 +0900

    feat: 프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://soupspoon.kr',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/oauth2': {
+        target: 'https://soupspoon.kr',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
