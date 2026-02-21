5c0147747206e4692151da7bde9005c54f81f805
MINJOO-KIM <gkminju2000@gmail.com>
2026-02-21 19:45:15 +0900

    chore: tanstack 기본설정 (QueryClientProvider로 감싸기)

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
-// src/main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import '@/app/styles/index.scss'
-import '@fontsource/pretendard'
+// import { AuthProvider } from '@/features/auth'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        {/* <AuthProvider> */}
+        <App />
+        {/* <AuthProvider> */}
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
