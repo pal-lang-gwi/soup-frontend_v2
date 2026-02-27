2853a83ab24d129b058407bf2802ece6802a0f38
MINJOO-KIM <gkminju2000@gmail.com>
2026-02-27 18:39:22 +0900

    feat: user 초기 정보 설정 api 예상 응답타입 추가

--- src/entities/user/api/initUser.ts
+++ src/entities/user/api/initUser.ts
@@ -6,7 +6,21 @@ interface InitUserParams {
   birthDate: string // "YYYY-MM-DD"
 }
 
-export const initUser = async (params: InitUserParams) => {
-  const response = await apiClient.post('/users/init', params)
-  return response.data
-}
+interface InitUserResponse {
+    success: boolean
+    data: {
+      userId: number
+      email: string
+      nickname: string
+      role: string
+      gender: string
+      birthDate: number[]
+      profileImageUrl: string
+    }
+    error: null | string
+  }
+  
+  export const initUser = async (params: InitUserParams): Promise<InitUserResponse> => {
+    const response = await apiClient.post<InitUserResponse>('/users/init', params)
+    return response.data
+  }
