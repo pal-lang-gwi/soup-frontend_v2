9e7c3ae56ec18b0f36eb44aba6f5b061c82c08b4
MINJOO-KIM <gkminju2000@gmail.com>
2026-03-02 22:42:37 +0900

    feat: 라우터 로직 수정

--- src/app/routers/router.tsx
+++ src/app/routers/router.tsx
@@ -1,37 +1,22 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { LandingPage } from '@/pages/landing'
-import { HomePage } from '@/pages/home'
 import { NewsPage } from '@/pages/news'
 import { AdminPage } from '@/pages/admin'
-// import { AdditionalInfoGuard } from '@/features/auth'
 
 const AppRoutes = () => (
   <Routes>
     <Route path='/' element={<LandingPage />} />
     <Route
-      path='/home'
-      element={
-        // <AdditionalInfoGuard>
-        <HomePage />
-        // </AdditionalInfoGuard>
-      }
-    />
-
-    <Route
       path='/news'
       element={
-        // <AdditionalInfoGuard>
         <NewsPage />
-        // </AdditionalInfoGuard>
       }
     />
 
     <Route
       path='/admin'
       element={
-        // <AdditionalInfoGuard>
         <AdminPage />
-        // </AdditionalInfoGuard>
       }
     />
 
