5693b3823c783c0359e995a9f38a27482bd4c22e
MINJOO-KIM <gkminju2000@gmail.com>
2026-04-15 00:11:31 +0900

    chore: Input 문구 수정

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -5,7 +5,7 @@ export const Input = () => {
   return (
     <div className={styles.input}>
       <img src={searchIcon} alt='검색' className={styles.searchIcon} />
-      <input type='text' className={styles.textInput} placeholder='키워드를 입력해보세요...' />
+      <input type='text' className={styles.textInput} placeholder='구독하고 싶은 키워드를 입력해보세요' />
     </div>
   )
 }
