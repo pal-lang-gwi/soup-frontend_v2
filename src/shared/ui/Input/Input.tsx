78de3bcf9fbf00d09b74c0b2adaf200437b19095
MINJOO-KIM <gkminju2000@gmail.com>
2026-04-17 12:13:59 +0900

    feat: Input 이벤트 추가

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,11 +1,41 @@
-import styles from '@/shared/ui/Input/Input.module.scss'
+import styles from './Input.module.scss'
 import searchIcon from '@/shared/assets/search_icon.png'
+import { useState, type ChangeEvent } from 'react'
+import { Button } from '@/shared/ui/Button/Button'
 
-export const Input = () => {
+interface InputProps {
+  onSubmit?: (value: string) => void;
+  placeholder?: string;
+}
+
+export const Input = ({ onSubmit, placeholder }: InputProps) => {
+  const [value, setValue] = useState('')
+
+  const handleAction = () => {
+    if (value.trim()) {
+      onSubmit?.(value); 
+      setValue('');
+    } else {
+      alert('구독할 키워드를 입력해주세요.');
+    }
+  }
+
   return (
-    <div className={styles.input}>
-      <img src={searchIcon} alt='검색' className={styles.searchIcon} />
-      <input type='text' className={styles.textInput} placeholder='구독하고 싶은 키워드를 입력해보세요!'/>
+    <div className={styles.inputContainer}>
+      <div className={styles.inputWrapper}>
+        <img src={searchIcon} alt='검색' className={styles.searchIcon} />
+        <input 
+          type='text' 
+          className={styles.textInput} 
+          placeholder={placeholder}
+          value={value}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') handleAction();
+          }}
+        />
+        <Button onClick={handleAction} size='s' typeStyle='type3'>검색</Button>
+      </div>
     </div>
   )
 }
